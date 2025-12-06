5b0f98ed972cb67d4d113d5502da14c265a5fa54
Uchiwa75567 <pabassdiame76@gmail.com>
2025-12-06 07:44:36 +0000

    reajustement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // Development environment settings
 export const environment = {
   production: false,
-  // backend JSON Server base URL
-  apiUrl: 'http://localhost:3004'
+  // backend JSON Server base URL (using production backend for both dev and prod)
+  apiUrl: 'https://json-server-senteranga.onrender.com'
 };
